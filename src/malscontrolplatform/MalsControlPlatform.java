5ce78e9f613d27a9a4116a0ef1abf5c1f2dd8068
Jakub Švarc <jakubsvarc@protonmail.com>
2021-12-25 10:53:29 +0100

    feature: added main class

---
+++ src/malscontrolplatform/MalsControlPlatform.java
@@ -0,0 +1,16 @@
+package malscontrolplatform;
+
+/**
+ *
+ * @author atlas144
+ */
+public class MalsControlPlatform {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
