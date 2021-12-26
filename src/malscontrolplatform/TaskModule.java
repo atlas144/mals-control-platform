75e2aa4d103c51a4912ba7a859b230dd85a3dd07
Jakub Švarc <jakubsvarc@protonmail.com>
2021-12-26 23:55:08 +0100

    feature: WebModule added to the TaskModule

--- src/malscontrolplatform/TaskModule.java
+++ src/malscontrolplatform/TaskModule.java
@@ -3,6 +3,7 @@ package malscontrolplatform;
 import java.util.concurrent.PriorityBlockingQueue;
 import malscontrolplatform.intercommunicationmodule.IntercommunicationModule;
 import malscontrolplatform.intercommunicationmodule.Message;
+import malscontrolplatform.webmodule.WebModule;
 
 /**
  *
@@ -12,12 +13,18 @@ public abstract class TaskModule extends Thread {
     
     private final String moduleName;
     protected final IntercommunicationModule intercommunicationModule;
+    protected final WebModule webModule;
     protected final PriorityBlockingQueue<Message> messageQueue;
         
-    public TaskModule(String moduleName, IntercommunicationModule intercommunicationModule) {
+    public TaskModule(String moduleName, IntercommunicationModule intercommunicationModule, WebModule webModule) {
         this.moduleName = moduleName;
         this.intercommunicationModule = intercommunicationModule;
+        this.webModule = webModule;
         messageQueue = new PriorityBlockingQueue<>();
+    }
+
+    public String getModuleName() {
+        return moduleName;
     }
     
     public void acceptMessage(Message message) {
@@ -26,9 +33,5 @@ public abstract class TaskModule extends Thread {
     
     @Override
     public abstract void run();
-
-    public String getModuleName() {
-        return moduleName;
-    }
     
 }
