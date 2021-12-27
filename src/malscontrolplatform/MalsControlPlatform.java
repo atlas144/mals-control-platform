34384acc5a6439d015cc2158d092ec8eaa81eb97
Jakub Švarc <jakubsvarc@protonmail.com>
2021-12-27 15:51:21 +0100

    feature: added basic structure to main class

--- src/malscontrolplatform/MalsControlPlatform.java
+++ src/malscontrolplatform/MalsControlPlatform.java
@@ -1,16 +1,36 @@
 package malscontrolplatform;
 
+import java.util.concurrent.ConcurrentHashMap;
+import malscontrolplatform.intercommunicationmodule.IntercommunicationModule;
+import malscontrolplatform.webmodule.WebModule;
+
 /**
  *
  * @author atlas144
  */
 public class MalsControlPlatform {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private final ConcurrentHashMap<String, TaskModule> taskModules;
+    private final IntercommunicationModule intercommunicationModule;
+    private final WebModule webModule;
+    
+    public MalsControlPlatform(String host, short port) {
+        taskModules = new ConcurrentHashMap<>();
+        intercommunicationModule = new IntercommunicationModule();
+        webModule = new WebModule(host, port);
+    }
+    
+    public void registerModule(TaskModule newModule) {
+        taskModules.put(newModule.getModuleName(), newModule);
+    }
+    
+    public void start() {
+        intercommunicationModule.start();
+        webModule.run();
+        
+        for (TaskModule taskModule : taskModules.values()) {
+            taskModule.start();
+        }
     }
     
 }
